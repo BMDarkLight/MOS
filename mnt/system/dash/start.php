4fd8b505f3e4948dc7b85974a47d4b1cdaf5ef21
BMDarkLight <72068517+BMDarkLight@users.noreply.github.com>
2020-09-29 19:34:43 +0330

    Add files via upload

---
+++ mnt/system/dash/start.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Dash Control Center</title>
+    </head>
+    <body>
+        <h1>Dash / Dash Control Center</h1>
+        Dash Name : Cross-Platform (Read-only)<br />
+        Dash Room : system://dash (No-Access)<br />
+        Dash Plug : system://dash/plugins (Read-write)<br />
+        <h3>Dash / Dash Control Center / How to Use?</h3>
+        Enter dash() Command In Terminal to See Features Of This Command.<br />
+        <div style="background-color: black;color: white;">
+            terminal:> dash() <br />
+            Dash Copyright (c) All Resvards 2018<br />
+            <code>dash_version() || dash()</code> See Features And Informations Of Dash<br />
+            <code>dash_install(&xcirc;Plugin Address&xcirc;) || dash_plug(&xcirc;Plugin Address&xcirc;)</code> Install Plugins<br />
+            <code>dash_set(&xcirc;Name&xcirc;,&xcirc;value&xcirc;)</code> Set a Variable<br />
+            <code>reboot() || redash()</code> Do Restart<br />
+            <code>shdn() || shdash()</code> Do Shut Down<br />
+            <code>dash_run(&xcirc;PHP Command&xcirc;) || php(&xcirc;PHP Command&xcirc;)</code> Run a PHP Command (Not Recommended Use This)<br />
+            <code>runos() || dash_os()</code> Run Default Cross Platform OS.<br />
+            mos_dash v1.2 (responsity main/dash) open source<br />
+            Dash Manual (responsity main/dashs/man) (Build 9)<br />
+            Cross-Platform Terminal<br />
+        </div>
+    </body>
+</html>
